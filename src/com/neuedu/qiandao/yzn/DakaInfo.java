f1b5d3b06fe449770561223c82e9f905d61c4576
user250 <user250@LAPTOP-E70H9MOM>
2019-07-18 10:48:48 +0800

    631

---
+++ src/com/neuedu/qiandao/yzn/DakaInfo.java
@@ -0,0 +1,21 @@
+package com.neuedu.qiandao.yzn;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * ´ò¿¨ÐÅÏ¢
+ * @author ttc
+ *
+ */
+public class DakaInfo implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public int id;
+	public Date qdTime;
+	public Date qtTime;
+	
+
+}
